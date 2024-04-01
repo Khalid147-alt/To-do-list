43a7556f9eaf8cf26ff44870229e56ca79d71e8e
Khalid147-alt <khalidunar103@gmail.com>
2024-04-01 23:03:09 +0500

    first commit

---
+++ alpha.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+// Import the inquirer module from the node_modules folder
+import inquirer from "inquirer";
+// Initialize an empty array to store the to-do tasks
+let toDo = [];
+// let toDo = [];
+// Initialize a variable to control the while loop
+let condition = true;
+// Define an async function to run the program
+async function main() {
+    // Use a while loop to keep running the program until the user decides to stop
+    while (condition) {
+        // Use await to wait for the user's response to the prompt
+        let addTask = await inquirer.prompt([
+            {
+                // Define the first prompt: ask the user to enter a task
+                name: "to do",
+                type: "input",
+                message: "what you want to add in your to do ? : ",
+            },
+            {
+                // Define the second prompt: ask the user if they want to add more tasks
+                name: "add more",
+                type: "confirm",
+                message: "do you want to add more ? ",
+                default: "fasle",
+            },
+        ]);
+        // Add the user's task to the toDo array
+        toDo.push(addTask["to do"]);
+        // Update the condition variable based on the user's response to the second prompt
+        condition = addTask["add more"];
+        // Print the updated toDo array
+        console.log(toDo);
+    }
+    ;
+}
+;
+// Call the main function to start the program
+main();
